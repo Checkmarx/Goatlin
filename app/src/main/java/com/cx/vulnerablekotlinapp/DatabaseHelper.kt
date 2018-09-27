96e6f95154712faae4c3668715e75d0488e44547
lopesg <isec.gls@gmail.com>
2018-09-27 11:04:12 +0200

    Source code first version

---
+++ app/src/main/java/com/cx/vulnerablekotlinapp/DatabaseHelper.kt
@@ -0,0 +1,90 @@
+package com.cx.vulnerablekotlinapp
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import java.io.File
+import java.io.FileOutputStream
+
+class DatabaseHelper (val context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    private val preferences: SharedPreferences = context.getSharedPreferences(
+            "${context.packageName}.database_versions",
+            Context.MODE_PRIVATE
+    )
+
+    private fun installedDatabaseIsOutdated(): Boolean {
+        return preferences.getInt(DATABASE_NAME, 0) < DATABASE_VERSION
+    }
+
+    private fun writeDatabaseVersionInPreferences() {
+        preferences.edit().apply {
+            putInt(DATABASE_NAME, DATABASE_VERSION)
+            apply()
+        }
+    }
+
+    private fun installDatabaseFromAssets() {
+        val inputStream = context.assets.open("$ASSETS_PATH/$DATABASE_NAME.sqlite3")
+
+        try {
+            val outputFile = File(context.getDatabasePath(DATABASE_NAME).path)
+            val outputStream = FileOutputStream(outputFile)
+
+            inputStream.copyTo(outputStream)
+            inputStream.close()
+
+            outputStream.flush()
+            outputStream.close()
+        } catch (exception: Throwable) {
+            throw RuntimeException("The $DATABASE_NAME database couldn't be installed.", exception)
+        }
+    }
+
+    @Synchronized
+    private fun installOrUpdateIfNecessary() {
+        if (installedDatabaseIsOutdated()) {
+            context.deleteDatabase(DATABASE_NAME)
+            installDatabaseFromAssets()
+            writeDatabaseVersionInPreferences()
+        }
+    }
+
+    public fun userLogin(username: String , password: String) : Boolean {
+        var ret = "select * from ${TABLE_ACCOUNTS} where username = '" + username + "' and password = '" + password + "';"
+        var cursor: Cursor = this.readableDatabase.rawQuery(ret, null)
+        if(cursor.count == 1)
+        {
+            return true
+        }
+        return false
+    }
+
+    override fun getWritableDatabase(): SQLiteDatabase {
+        throw RuntimeException("The $DATABASE_NAME database is not writable.")
+    }
+
+    override fun getReadableDatabase(): SQLiteDatabase {
+        installOrUpdateIfNecessary()
+        return super.getReadableDatabase()
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        // Nothing to do: Database already created
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        // Nothing to do: Database already created
+    }
+
+    companion object {
+        const val ASSETS_PATH = "database"
+        const val DATABASE_NAME = "data"
+        const val DATABASE_VERSION = 2
+        const val TABLE_ACCOUNTS = "Accounts"
+    }
+
+
+}
