6045070846181e6ac9b7e7f4880c87da051a0850
PauloASilva <pauloasilva@gmail.com>
2018-12-16 17:18:27 +0000

    refactor: enable Certificate Pinning on Android client

--- packages/clients/android/app/src/main/java/com/cx/vulnerablekotlinapp/api/service/Client.kt
+++ packages/clients/android/app/src/main/java/com/cx/vulnerablekotlinapp/api/service/Client.kt
@@ -1,6 +1,8 @@
 package com.cx.vulnerablekotlinapp.api.service
 
 import com.cx.vulnerablekotlinapp.api.model.Account
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -14,10 +16,19 @@ interface Client {
 
     companion object {
         fun create(): Client {
+            val certificatePinner = CertificatePinner.Builder()
+                    .add("192.169.1.87:8080", "sha256/5Kl14sIBRoArZ8ujwNLWoLOI1QmsvE58nmXTO/9GSJw=")
+                    .build()
+
+            val client: OkHttpClient = OkHttpClient.Builder()
+                    .certificatePinner(certificatePinner)
+                    .build()
+
             val retrofit = Retrofit.Builder()
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl("http://192.168.1.87:8080")
+                    .baseUrl("https://192.168.1.87:8080")
+                    .client(client)
                     .build()
 
             return retrofit.create(Client::class.java)
