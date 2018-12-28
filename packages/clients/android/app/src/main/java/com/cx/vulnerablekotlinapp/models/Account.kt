b76363febd417de50efbfece9b6398453e914e4e
PauloASilva <pauloasilva@gmail.com>
2018-12-28 19:28:24 +0000

    refactor(app): explicit password comparison on LoginActivity
    This change makes hashed password comparison (M4) easier and more
    clear.
    
    * Account model (data class) added
    * DatabaseHelper.userLogin() method removed

---
+++ packages/clients/android/app/src/main/java/com/cx/vulnerablekotlinapp/models/Account.kt
@@ -0,0 +1,14 @@
+package com.cx.vulnerablekotlinapp.models
+
+import android.database.Cursor
+
+data class Account(var username: String, var password: String) {
+    var id: Int = -1
+
+    constructor(cursor: Cursor) : this(
+            cursor.getString(cursor.getColumnIndex("username")),
+            cursor.getString(cursor.getColumnIndex("password"))
+    ){
+        id = cursor.getInt(cursor.getColumnIndex("id"))
+    }
+}
