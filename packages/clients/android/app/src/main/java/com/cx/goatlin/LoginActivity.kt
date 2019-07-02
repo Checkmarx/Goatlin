8e0add9ae0309b8432317033f23c5d9e0b40f595
PauloASilva <pauloasilva@gmail.com>
2019-07-02 14:27:20 +0100

    fix(app): login

--- packages/clients/android/app/src/main/java/com/cx/goatlin/LoginActivity.kt
+++ packages/clients/android/app/src/main/java/com/cx/goatlin/LoginActivity.kt
@@ -223,14 +223,16 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
                 try {
                     val account: Account = DatabaseHelper(applicationContext).getAccount(mUsername)
 
-                    if (mPassword == account.password) {
-                        val prefs: SharedPreferences = applicationContext.getSharedPreferences(
-                                applicationContext.packageName, Context.MODE_PRIVATE)
-                        val editor: SharedPreferences.Editor = prefs.edit()
-
-                        editor.putInt("userId", account.id).apply()
-                        editor.putString("userEmail", mUsername).apply()
+                    if (mPassword != account.password) {
+                        return false;
                     }
+
+                    val prefs: SharedPreferences = applicationContext.getSharedPreferences(
+                            applicationContext.packageName, Context.MODE_PRIVATE)
+                    val editor: SharedPreferences.Editor = prefs.edit()
+
+                    editor.putInt("userId", account.id).apply()
+                    editor.putString("userEmail", mUsername).apply()
 
                     return account.id > -1
                 } catch(e: Exception){
